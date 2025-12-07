4c8d79ef08780082c2f5272e44ffd1f31af0e6c1
joseph <tobij337@gmail.com>
2025-12-07 18:49:30 +0100

    Worked on the while loop, created history list, Added Show tasks tool

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.tools import tool
 from langchain.agents import create_openai_tools_agent, AgentExecutor
+from todoist_api_python.api import TodoistAPI
 
 
 load_dotenv()
@@ -16,19 +17,34 @@ load_dotenv()
 todoist_api_key = os.getenv("TODOIST_API_KEY")
 gemini_api_key = os.getenv("GEMINI_API_KEY")
 
+todoist = TodoistAPI(todoist_api_key)
+
 # tool is a python function in the langchain jargon that directs the llm to run
 # an action, in this case add_task(). A docstring explaining the function is
 # compulsory as it gives the llm context
+# Desc(Description) is added by default by the llm id you do not give it a default value
 @tool
-def add_task():
+def add_task(task, desc=None):
     """Add a new task to the user's task list. Use this when the user wants to
     add or create a task
     """
-    print("Adding Task")
-    print("Added task")
+    todoist.add_task(content=task,
+                     description=desc)
+
+@tool
+def show_tasks():
+    """Show all tasks from Todoist. Use this tool when the user wants to see their
+    tasks"""
+    results_paginator = todoist.get_tasks()
+    tasks = []
+    for task_list in results_paginator:
+        for task in task_list:
+            tasks.append(task.content)
+
+    return tasks
 
 # The tools have to be added to a list
-tools = [add_task]
+tools = [add_task, show_tasks]
 
 llm = ChatGoogleGenerativeAI(
     model='gemini-2.5-flash',
@@ -36,20 +52,22 @@ llm = ChatGoogleGenerativeAI(
     temperature=0.3
 )
 # System prompt to give the llm perspective as to its purpose
-system_prompt = "You are a helpful assistant. You will help the user manage tasks"
-user_prompt = "Add a new task to buy benz"
+system_prompt = """You are a helpful assistant. You will help the user manage tasks.
+if the user asks to show all tasks: for example: "show all tasks", print out all the 
+tasks to the user. Print them in a  bullet list format.
+"""
+
 
 prompt = ChatPromptTemplate([
     ("system", system_prompt),
-    ("user", user_prompt),
+    MessagesPlaceholder("history"),
+    ("user", "{input}"),
     MessagesPlaceholder("agent_scratchpad")
 ])
 
 
 agent =  create_openai_tools_agent(llm, tools, prompt)
 agent_execute = AgentExecutor(agent=agent, tools=tools, verbose=False)
-
-response = agent_execute.invoke({"input": user_prompt})
 
 # Chain is a variable to carry the code for langchain, it is where it got its name
 # as commands pass to it look like a chain, separated with |, following the chain
@@ -59,6 +77,13 @@ response = agent_execute.invoke({"input": user_prompt})
 # To run the llm, .invoke takes a dictionary as an argument with input as key
 # response = chain.invoke({"input": user_prompt})
 
-print(response)
-
+# To keep track of previous conversation, the history of the conversation is
+# appended to the list
+history = []
+while True:
+    user_prompt = input("You: ")
+    response = agent_execute.invoke({"input": user_prompt, "history":history})
+    print(response["output"])
+    history.append(HumanMessage(content=user_prompt))
+    history.append(AIMessage(content=response["output"]))
 
